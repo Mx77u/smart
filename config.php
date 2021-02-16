ee91c935bbc10c28660e404cfc1dfae3b0c1f65a
krojo-masterofworld <59960346+krojo-masterofworld@users.noreply.github.com>
2021-02-16 08:40:02 +0100

    Add files via upload
    simple login system

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'smartsoftware_admin');
+define('DB_PASSWORD', 'cooleschule247');
+define('DB_NAME', 'smartsoftware_users');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
